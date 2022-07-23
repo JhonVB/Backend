7dec4165658bc4a47050f9a359fd90e4445d4a82
JhonVB <jhonba2020@gmail.com>
2022-07-23 12:15:50 -0500

    Config

--- enersinc/settings.py
+++ enersinc/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY',get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG","FALSE") == "True"
 
-ALLOWED_HOSTS = os.getenv("*")
+ALLOWED_HOSTS = ["*"]
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
